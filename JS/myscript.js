930cf6b8d806318226da4f4eeb33620aaf8681dc
Chiaramartina <106493111+Chiaramartina@users.noreply.github.com>
2024-09-12 21:13:10 +0200

    index.html, homepage

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,55 +1,87 @@
-var $animation_elements = $('.slide');
-    var $window = $(window);
-    function check_if_in_view() {
-      var window_height = $window.height();
-      var window_top_position = $window.scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) $element.addClass('hasSlid');
-        else $element.removeClass('hasSlid');
-      });
-    }
-    $window.on('scroll resize', check_if_in_view);
+const cardWrapper = document.querySelector('.card-wrapper')
+const cardWrapperChildren = Array.from(cardWrapper.children)
+const widthToScroll = cardWrapper.children[0].offsetWidth
+const arrowPrev = document.querySelector('.arrow.prev')
+const arrowNext = document.querySelector('.arrow.next')
+const cardBounding = cardWrapper.getBoundingClientRect()
+const column = Math.floor(cardWrapper.offsetWidth / (widthToScroll + 24))
+let currScroll = 0
+let initPos = 0
+let clicked = false
 
+cardWrapperChildren.slice(-column).reverse().forEach(item=> {
+  cardWrapper.insertAdjacentHTML('afterbegin', item.outerHTML)
+})
 
-    $(".step").click( function() {
-      $(this).addClass("active").prevAll().addClass("active");
-      $(this).nextAll().removeClass("active");
-    });
-    
-    $(".step01").click( function() {
-      $("#line-progress").css("width", "3%");
-      $(".discovery").addClass("active").siblings().removeClass("active");
-    });
-    
-    $(".step02").click( function() {
-      $("#line-progress").css("width", "25%");
-      $(".strategy").addClass("active").siblings().removeClass("active");
-    });
-    
-    $(".step03").click( function() {
-      $("#line-progress").css("width", "50%");
-      $(".creative").addClass("active").siblings().removeClass("active");
-    });
-    
-    $(".step04").click( function() {
-      $("#line-progress").css("width", "75%");
-      $(".production").addClass("active").siblings().removeClass("active");
-    });
-    
-    $(".step05").click( function() {
-      $("#line-progress").css("width", "100%");
-      $(".analysis").addClass("active").siblings().removeClass("active");
-    });
-    
-    $("#color").click( function() {
-      $("body").toggleClass("blue")
-    });
+cardWrapperChildren.slice(0, column).forEach(item=> {
+  cardWrapper.insertAdjacentHTML('beforeend', item.outerHTML)
+})
 
+const cardImageAndLink = cardWrapper.querySelectorAll('img, a')
+cardImageAndLink.forEach(item=> {
+  item.setAttribute('draggable', false)
+})
+
+cardWrapper.classList.add('no-smooth')
+cardWrapper.scrollLeft = cardWrapper.offsetWidth
+cardWrapper.classList.remove('no-smooth')
+
+arrowPrev.onclick = function() {
+  cardWrapper.scrollLeft -= widthToScroll
+}
+
+arrowNext.onclick = function() {
+  cardWrapper.scrollLeft += widthToScroll
+}
+
+cardWrapper.onmousedown = function(e) {
+  cardWrapper.classList.add('grab')
+  initPos = e.clientX - cardBounding.left
+  currScroll = cardWrapper.scrollLeft
+  clicked = true
+}
+
+cardWrapper.onmousemove = function(e) {
+  if(clicked) {
+    const xPos = e.clientX - cardBounding.left
+    cardWrapper.scrollLeft = currScroll + -(xPos - initPos)
+  }
+}
+
+cardWrapper.onmouseup = mouseUpAndLeave
+cardWrapper.onmouseleave = mouseUpAndLeave
+
+function mouseUpAndLeave() {
+  cardWrapper.classList.remove('grab')
+  clicked = false
+}
+
+let autoScroll
+
+cardWrapper.onscroll = function() {
+  if(cardWrapper.scrollLeft === 0) {
+    cardWrapper.classList.add('no-smooth')
+    cardWrapper.scrollLeft = cardWrapper.scrollWidth - (2 * cardWrapper.offsetWidth)
+    cardWrapper.classList.remove('no-smooth')
+  } else if(cardWrapper.scrollLeft === cardWrapper.scrollWidth - cardWrapper.offsetWidth) {
+    cardWrapper.classList.add('no-smooth')
+    cardWrapper.scrollLeft = cardWrapper.offsetWidth
+    cardWrapper.classList.remove('no-smooth')
+  }
+
+  if(autoScroll) {
+    clearTimeout(autoScroll)
+  }
+
+  autoScroll = setTimeout(()=> {
+    cardWrapper.classList.remove('no-smooth')
+    cardWrapper.scrollLeft += widthToScroll
+  }, 4000)
+}
+
+/* scrollin*/
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 
