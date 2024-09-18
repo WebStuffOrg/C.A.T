36c40571cdd680bcc59c9a4a5d323ec5c4a02510
valetedd <vale98h@gmail.com>
2024-09-18 11:39:34 +0200

    JS and JSON modified, layout to be modified

--- JS/narr.js
+++ JS/narr.js
@@ -1,51 +1,109 @@
 let currentNarrative = []
+let narrativeTitle = ""
 let currentIdx = 0
-let currentArtwork = currentNarrative[currentIdx]
+let items = []
+let narratives = []
+
+const nextButton = document.getElementById("next-button")
+const backButton = document.getElementById("back-button")
 
 
 document.addEventListener("DOMContentLoaded", () => {
 	fetch('data/narr.json')
 	.then(response => response.json())
-	.then(data => {
-        defaultNarrative = data.meta.defaultNarrative
+	.then(async data => {
+        items = data.items
+        narratives = data.narratives
         currentIdx = data.meta.defaultStart
-        currentNarrative = data['narratives'][defaultNarrative]['all']
-		currentArtwork = currentNarrative[currentIdx]
+        narrativeTitle = data.meta.defaultNarrative
+        currentNarrative = data.narratives[narrativeTitle] 
+        itemData = items[currentNarrative[currentIdx]]
 
-        setContent(currentArtwork)
-	})
+        await setContent(itemData)
+        setDropdownList(currentNarrative, currentIdx)
+        if (currentIdx === 0) backButton.disabled = true
+        document.getElementById('current-narrative').innerHTML = narrativeTitle
+    })
 });
 
-function nextArtwork(e) {
+
+async function nextItem() {
     if (currentIdx === 0) {
-        document.getElementById('prev-button').disabled = false
+        backButton.disabled = false
     }
     currentIdx += 1
+    await setContent(items[currentNarrative[currentIdx]])
     if (currentIdx === currentNarrative.length - 1) {
-        e.disabled = true
+        nextButton.disabled = true
     };
-    setContent(currentArtwork)
 };
 
-function prevArtwork(e) {
+async function prevItem() {
     if (currentIdx === currentNarrative.length - 1) {
-        document.getElementById('next-button').disabled = false
+        nextButton.disabled = false
     }
     currentIdx -= 1
+    await setContent(items[currentNarrative[currentIdx]])
     if (currentIdx === 0) {
-        e.disabled = true
+        backButton.disabled = true
     }
-    setContent(currentArtwork)
 };
 
-function setContent(id) {
-    ;
+async function setContent(data) {
+    await loadImage(data.img)
+    document.querySelectorAll('.current-artwork').forEach((el) => {
+        el.innerHTML = data.title
+    });
+    document.querySelectorAll('.table-data').forEach((el) => {
+        el.innerHTML = data[el.id]
+    });
 };
-function setDropdownList(currentNarrative) {
-    ;
+
+async function loadImage(imagePath) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = imagePath;
+        img.onload = () => {
+            document.getElementById('artwork-img').setAttribute('src', imagePath);
+            resolve();
+        };
+        img.onerror = () => reject(new Error('Image failed to load'));
+    });
+};
+
+async function setDropdownList(narrative=currentNarrative) {
+    const dropdown = document.getElementById('dropdown-list')
+    narrative.forEach((item, i) => {
+        const listElement = document.createElement('li')
+        listElement.classList.add('dropdown-item')
+        listElement.id = i
+        listElement.innerHTML = i+1 + ". " + items[item].title
+        listElement.onclick = async () => {
+            currentIdx = i
+            await setContent(items[item])
+        }
+        dropdown.appendChild(listElement)
+    })
+    disableDropItem(currentIdx);
 }
 
-function switchNarrative (narr) {
-   ;
+
+function disableDropItem(idx) {
+    document.querySelector('.disabled')?.classList.remove('disabled')
+    const dropdownItem = document.getElementById(`${idx}`)
+    dropdownItem.classList.add('disabled')
+    }
+
+async function switchNarrative (narrative, id=null) {
+    currentNarrative = narratives[narrative];
+    if (id) currentIdx = currentNarrative.indexOf(id)
+    const currItem = items[currentNarrative[currentIdx]]
+   document.getElementById('current-narrative').innerHTML = narrative
+   await setContent(currItem)
+   if (currentIdx === 0) backButton.disabled = true;
+   else {
+    backButton.disabled = false;
+    if (currentIdx === currentNarrative.lenght - 1) nextButton.disabled = true    
+   } 
 }
 
