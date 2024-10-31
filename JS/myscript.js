d29eca2387ad6f1ad23a81aff0535612fbed4503
Chiaramartina <106493111+Chiaramartina@users.noreply.github.com>
2024-10-31 10:37:11 +0100

    cleaning

--- JS/myscript.js
+++ JS/myscript.js
@@ -6,24 +6,4 @@ function scrollToTop() {
 
 AOS.init();
 
-document.addEventListener('DOMContentLoaded', function() {
-  var video = document.getElementById('video-back');
 
-  function playVideo() {
-    video.play().catch(error => {
-      console.log('Autoplay bloccato dal browser:', error);
-    });
-  }
-
-  // Check if the screen is in responsive mode (< 768px) to autoplay the video
-  if (window.innerWidth <= 768) {
-    playVideo();
-  }
-
-  // Optional: Also listen for window resize to restart video if resizing below 768px
-  window.addEventListener('resize', function() {
-    if (window.innerWidth <= 768) {
-      playVideo();
-    }
-  });
-});
